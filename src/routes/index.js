9becb7e2f8e998dee24e0e1ad7bdd338262a0bb5
Jobana Garavito Q <nellyjobana@gmail.com>
2020-08-11 14:38:29 -0500

    SPA cientifico- Platzi Proyect

---
+++ src/routes/index.js
@@ -0,0 +1,26 @@
+import Header from '../templates/header'
+import Home from '../pages/home'
+import Character from '../pages/character'
+import error404 from '../pages/error404'
+import getHash from '../utils/getHash'
+import resolveRoutes from '../utils/resolveRoutes'
+
+const routes = {
+    '/': Home,
+    '/:id': Character,
+    '/contact': 'Contact',
+}
+
+const Router = async () => {
+    const header = null || document.getElementById('header')
+    const content = null || document.getElementById('content')
+
+    header.innerHTML = await Header();
+
+    let hash = getHash()
+    let route = await resolveRoutes(hash)
+    let render = routes[route] ? routes[route] : error404;
+    content.innerHTML = await render()
+}
+
+export default Router
